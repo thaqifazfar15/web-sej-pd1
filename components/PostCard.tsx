692da537392348333814fbacad08336e595fcd05
Thaqif Azfar <thaqifazfar15@gmail.com>
2022-03-29 00:20:14 +0800

    Update Postcatd UI

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,10 +1,66 @@
 import React from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
 function PostCard({ post }: any) {
+  console.log(post)
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
+      <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="absolute h-80 w-full rounded-t-lg object-cover object-top shadow-lg lg:rounded-lg"
+        />
+      </div>
+      <h1
+        className="cursor:pointer mb-8 text-center text-3xl font-semibold
+      transition duration-100 hover:text-pink-600"
+      >
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
+      </h1>
+      <div className="mb-8 block w-full items-center justify-center text-center lg:flex">
+        <div className="mb-4 mr-8 flex w-full items-center justify-center lg:mb-0 lg:w-auto">
+          <img
+            src={post.author.photo.url}
+            alt={post.author.name}
+            height="30px"
+            width="30px"
+            className="rounded-full align-middle"
+          />
+          <p className="ml-2 inline align-middle text-lg text-gray-700">
+            {post.author.name}
+          </p>
+        </div>
+        <div className="flex justify-center gap-2 text-gray-700 first-letter:font-medium">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+        </div>
+      </div>
+      <p className="mb-8 px-4 text-center text-lg font-normal text-gray-700 lg:px-20">
+        {post.excerpt}
+      </p>
+
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="inline-block transform cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:-translate-y-1">
+            Continue Reading
+          </span>
+        </Link>
+      </div>
     </div>
   )
 }
